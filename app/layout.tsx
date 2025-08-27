b4f10ddcdf1b230bff81af5cd3c8edc3f51ad389
Globastu <stuart@globapay.io>
2025-08-27 03:32:16 +0100

    feat: update layout, add StickyHeader and partner logos

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "next-themes";
+import StickyHeader from "@/components/StickyHeader";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+          <StickyHeader />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
